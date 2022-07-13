2f59962d3efd86220e1b095a78659b038c59c720
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2022-07-13 22:11:58 +0530

    July 13 2022 1022 PM

--- src/Main3.java
+++ src/Main3.java
@@ -10,30 +10,30 @@ public class Main3 {
     public static void main(String[] args) throws InterruptedException {
 
         ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
-       // ExecutorService executorService1 = Executors.newFixedThreadPool(10);
+        // ExecutorService executorService1 = Executors.newFixedThreadPool(10);
 
         Instant start = Instant.now();
 
         for (int value = 1; value < 101; value++) {
             int finalValue = value;
             executorService.submit(() -> {
-                System.out.println("Printing "+ finalValue +" "+Thread.currentThread());
-                int theVal=0;
+                System.out.println("Printing " + finalValue + " " + Thread.currentThread());
+                int theVal = 0;
                 try {
                     sleep(5000);
-                    theVal=getSame(finalValue);
+                    theVal = getSame(finalValue);
                 } catch (InterruptedException e) {
                     System.out.println("Virtual Thread Execution Exception");
                 }
-                System.out.println("Printing "+ theVal +" "+Thread.currentThread());
+                System.out.println("Printing " + theVal + " " + Thread.currentThread());
                 return theVal;
             });
         }
 
         executorService.shutdown();
-        executorService.awaitTermination(10,TimeUnit.MINUTES);
+        executorService.awaitTermination(10, TimeUnit.MINUTES);
         Instant end = Instant.now();
-        System.out.println(Duration.between(start,end).toSeconds());
+        System.out.println(Duration.between(start, end).toSeconds());
 
     }
 
