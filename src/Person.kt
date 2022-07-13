ed825b9f7e99b631a5aa93805c5d1147fdfc5716
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2022-07-13 22:00:40 +0530

    July 13 2022 10 PM

--- src/Person.kt
+++ src/Person.kt
@@ -1,15 +1,16 @@
-class Person(val firstName: String="Ravi", val lastName: String="Varma") {
+class Person(val firstName: String = "Ravi", val lastName: String = "Varma") {
 
     var nickName: String? = null
 
     init {
         println("Init 1")
     }
+
     init {
         println("Init 2")
     }
 
-    constructor(): this("Varma","Ravi"){
+    constructor() : this("Varma", "Ravi") {
         println("Secondary Construtor")
     }
 
@@ -17,7 +18,7 @@ class Person(val firstName: String="Ravi", val lastName: String="Varma") {
         return "Person(firstName='$firstName', lastName='$lastName', nickName=$nickName)"
     }
 
-    fun printInfo(){
+    fun printInfo() {
         val nickNameToPrint = nickName ?: "no nickName"
         println("$firstName ($nickNameToPrint) $lastName")
     }
